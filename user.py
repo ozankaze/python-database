87e97e838c06757eb2e7a6110e5bc7c314a0173d
Zan <ozankaze101@gmail.com>
2022-06-28 22:30:50 +0700

    insert data satu atau banyak

--- user.py
+++ user.py
@@ -10,3 +10,15 @@ class User(Model):
         database = db # This model uses the "people.db" database.
 
 db.connect()
+db.create_tables([User], safe = True) #untuk membuat table baru
+
+# insert satu data
+# User.create(name = 'Irine', birthday = 'Inchon')
+
+# insert banyak data
+# data_resource = [
+#     {'name' : 'Joy', 'birthday' : 'Soeul'},
+#     {'name' : 'Mira', 'birthday' : 'Soeul'}
+# ]
+
+# print(User.insert_many(data_resource).execute())
